daf8858b25f2a9bebed2fd6f5267dabbc2acf006
YoniIfrah <yonifrah996@gmail.com>
2023-04-30 23:51:04 +0300

    fixed auth

---
+++ app/components/modals/LoginModal.tsx
@@ -0,0 +1,83 @@
+"use client";
+import React, {useCallback, useState} from 'react'
+import {signIn} from 'next-auth/react'
+import axios from 'axios';
+import { AiFillGithub } from 'react-icons/ai';
+import {FcGoogle} from 'react-icons/fc'
+import {FieldValues, SubmitHandler, useForm} from 'react-hook-form'
+import useLoginModal from '@/app/hooks/userLoginModal';
+import Modal from './Modal';
+import Heading from '../Heading';
+import Input from '../inputs/Input';
+import { toast } from 'react-hot-toast';
+import Button from '../Button';
+import { useRouter } from 'next/navigation';
+
+
+
+const LoginModal = () => {
+    const router = useRouter()
+    const LoginModal = useLoginModal()
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const {register, handleSubmit, formState: {errors} } = useForm<FieldValues>({defaultValues:{email:'', password:''}})
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true)
+
+        /** data will be the obj from FieldValues */
+        signIn('credentials', {
+            ...data,
+            redirect: false
+        }).then((callback) => {
+            setIsLoading(false)
+
+            if(callback?.ok){
+                toast.success('Logged in')
+                router.refresh()
+                LoginModal.onClose()
+            }
+            if(callback?.error){
+                toast.error(callback.error)
+            }
+        })
+        }
+    const bodyContent = (
+        <div className='flex flex-col gap-4'>
+            <Heading title='Welcome Back' subtitle='Login to your account!' center={true}/>
+            <Input id='email' label='email' disabled={isLoading} register={register} errors={errors} required/>
+
+            <Input id='password' label='password' type='password' disabled={isLoading} register={register} errors={errors} required/>
+        </div>
+    )
+
+    const footerContent = (
+        <div className='flex flex-col gap-4 mt-3'>
+            <hr />
+            <Button outline label='Continue with Google' icon={FcGoogle} onClick={() => {}}/>
+            <Button outline label='Continue with Github' icon={AiFillGithub} onClick={() => {}}/>
+            <div className='text-neutral-500 text-center mt-4 font-light'>
+                <div className='justify-center flex flex-row items-center gap-2'>
+                    <div>
+                        Already have an account?
+                    </div>
+                    <div className=' text-neutral-800 cursor-pointer hover:underline' onClick={LoginModal.onClose}>
+                        Login
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+  return (
+    <Modal
+    disabled={isLoading} 
+    isOpen={LoginModal.isOpen} 
+    title='Login' 
+    actionLabel='Continue' 
+    onClose={LoginModal.onClose}
+    onSubmit={handleSubmit(onSubmit)}
+    body={bodyContent}
+    footer={footerContent}
+    />
+  )
+}
+
+export default LoginModal
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,8 +4,11 @@ import {AiOutlineMenu} from 'react-icons/ai'
 import Avatar from '../Avatar';
 import MenuItem from './MenuItem';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
+import useLoginModal from '@/app/hooks/userLoginModal';
 const UserMenu = () => {
     const registerModal = useRegisterModal()
+    const loginModal = useLoginModal()
+
     const [isOpen, setIsOpen] = useState<boolean>(false);
     const toggleOpen = useCallback(() => {
         setIsOpen((value) => !value)
@@ -73,7 +76,7 @@ const UserMenu = () => {
                 >
                 <div className="flex flex-col cursor-pointer">
                     <>
-                    <MenuItem onClick={()=> {}} label='Login'/>
+                    <MenuItem onClick={loginModal.onOpen} label='Login'/>
                     <MenuItem onClick={registerModal.onOpen} label='Sign up'/>
                     </>
                 </div>
