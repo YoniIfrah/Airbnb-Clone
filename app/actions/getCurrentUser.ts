9c3d9fb11f9e45fe37db14b2728910c46afcfc6c
YoniIfrah <yonifrah996@gmail.com>
2023-05-05 18:06:07 +0300

    fixed schema and deleted Cluster0 from mongo

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,34 +1,38 @@
-import {getServerSession} from 'next-auth/next'
-import { authOptions } from '@/pages/api/auth/[...nextauth]'
-import prisma from '@/app/libs/prismadb'
+import { getServerSession } from "next-auth/next"
 
-export  async function getSession() {
-    return await getServerSession(authOptions)
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import prisma from "@/app/libs/prismadb";
+
+export async function getSession() {
+  return await getServerSession(authOptions)
 }
 
 export default async function getCurrentUser() {
-    try {
-        const session = await getSession()
-        if (!session?.user?.email){
-            return null
-        }
+  try {
+    const session = await getSession();
 
-        const currentUser = await prisma.user.findUnique({
-            where:{
-                email: session.user.email as string
-            }
-        })
-        if (!currentUser){
-            return null
-        }
-        return {
-            ...currentUser,
-            createdAt:currentUser.createdAt.toISOString(),
-            updatedAt: currentUser.updatedAt.toISOString(),
-            emailVerified: currentUser.emailVerify?.toISOString() || null
-        }
-
-    } catch(e){
-        return null
+    if (!session?.user?.email) {
+      return null;
     }
+
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        email: session.user.email as string,
+      }
+    });
+
+    if (!currentUser) {
+      return null;
+    }
+
+    return {
+      ...currentUser,
+      createdAt: currentUser.createdAt.toISOString(),
+      updatedAt: currentUser.updatedAt.toISOString(),
+      emailVerified: 
+        currentUser.emailVerified?.toISOString() || null,
+    };
+  } catch (error: any) {
+    return null;
+  }
 }
