3b9e876c28025fe90dd84614a9e9b31f797bccea
YoniIfrah <yonifrah996@gmail.com>
2023-07-07 11:16:43 +0300

    added step 4

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,8 @@ import { FieldValues, useForm } from 'react-hook-form';
 import CountrySelect from '../inputs/CountrySelect';
 import Map from '../Map'
 import dynamic from 'next/dynamic';
+import Counter from '../inputs/Counter';
+import ImageUpload from '../inputs/ImageUpload';
 
 enum STEPS {
     CATEGORY = 0,
@@ -48,6 +50,10 @@ function RentModal() {
 
     const category = watch('category')
     const location = watch('location')
+    const guestCount = watch('guestCount')
+    const roomCount = watch('roomCount')
+    const bathroomCount = watch('bathroomCount')
+    const imageSrc = watch('imageSrc')
 
     const Map = useMemo(() => dynamic(() => import('../Map'), {
       ssr: false
@@ -125,6 +131,62 @@ function RentModal() {
         />
         <Map center={location?.latlng} />
       </div>
+    );
+  }
+  if (step === STEPS.INFO){
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+      <Heading
+        title="Where is your place located?"
+        subtitle="Help guests find you!"
+      />
+      <Counter
+      title= "Guests"
+      subtitle="how many guests do you allow?"
+      value={guestCount}
+      onChange={(value) => setCustomValue('guestCount', value)}
+      />
+      <hr />
+      <Counter
+      title= "Rooms"
+      subtitle="how many rooms do you have?"
+      value={roomCount}
+      onChange={(value) => setCustomValue('roomCount', value)}
+      />
+      <hr />
+      <Counter
+      title= "Bathrooms"
+      subtitle='how many bathrooms do you have?'
+      value={bathroomCount}
+      onChange={(value) => setCustomValue('bathroomCount', value)}
+      />
+        
+      </div>
+    );
+  }
+  if (step === STEPS.IMAGES){
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+      <Heading
+        title="Add a photo of your place"
+        subtitle="Show guest what your place looks like!"
+      />
+      <ImageUpload
+      value={imageSrc}
+      onChange={(value) => setCustomValue('imageSrc', value)}
+      />
+      
+      </div>
+    );
+  }
+  if (step === STEPS.DESCRIPTION){
+    bodyContent = (
+      <div>DESCRIPTION</div>
+    );
+  }
+  if (step === STEPS.PRICE){
+    bodyContent = (
+      <div>PRICE</div>
     );
   }
 
